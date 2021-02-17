5d468d1564fb3c456b284a7189a469cf84e4263c
sakamoto <fabiojapa@gmail.com>
2021-02-17 18:51:09 -0300

    error test

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('CANARY-SAKA - Hellooo v34\n');
-}).listen(3000);
+const express = require("express");
+const app = express();
+const port = 3000;
+let countError = 0;
 
-console.log('Server running at http://127.0.0.1:3000/');
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getTest(req, res) {
+    if (getRandomInt(2) === 0) {
+        res.send("Hello World!");
+    } else {
+        res.status(500).send("error");
+        console.log('error', countError++);
+    }
+}
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/test", getTest);
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
+
+console.log("Server running at http://127.0.0.1:3000/");
