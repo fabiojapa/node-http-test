062430afdf6da1f9c9664b18706637945ef4de74
Fabio Sakamoto <fabiojapa@gmail.com>
2021-02-25 21:00:46 -0300

    dummy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function getTest(req, res) {
 }
 
 app.get("/", (req, res) => {
-    res.send("CANARY-SAKA - Ola Mundo v4");
+    res.send("CANARY-SAKA - Ola Mundo v4sss");
 });
 
 app.get("/test", getTest);
