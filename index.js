d2a9135efadb809d3ac04fb03c2ac311886c5508
Fabio Sakamoto <fabiojapa@gmail.com>
2021-03-22 09:53:19 -0300

    fix: del key and crt

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const options = {
 
 https.createServer(options, (req, res) => {
   res.writeHead(200);
-  res.end('hello world HTTPS\n');
+  res.end('CANARY HTTPS V2\n');
 }).listen(3000);
 
 console.log('Server running at https://127.0.0.1:3000/');
