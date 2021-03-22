cedaa02f2a26f607e528ba45799370107305c77d
Fabio Sakamoto <fabiojapa@gmail.com>
2021-03-22 09:34:58 -0300

    feat: over HTTPS with nsqa-ingress-tls.crt

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --no-cache -f ./Dockerfile -t fabiojapa/node-http-test:1.0.2test .
-docker push fabiojapa/node-http-test:1.0.2test
+docker build --no-cache -f ./Dockerfile -t fabiojapa/node-http-test:1.0.1-https .
+docker push fabiojapa/node-http-test:1.0.1-https
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('CANARY-SAKA - Hellooo v34\n');
+// curl -k https://localhost:3000/
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('nsqa-ingress-tls.key'),
+  cert: fs.readFileSync('nsqa-ingress-tls.crt')
+};
+
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('hello world HTTPS\n');
 }).listen(3000);
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at https://127.0.0.1:3000/');
